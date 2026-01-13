d895fbabf2d0578b5a8f26cf863fbea38b4f21a7
finotremy-a11y <finotremy@gmail.com>
2026-01-13 15:29:37 +0100

    exo 00 hello

---
+++ 00_hello.rb
@@ -0,0 +1,12 @@
+def ask_first_name
+  print "Quel est ton prénom ? "
+  first_name = gets.chomp
+  return first_name
+end
+
+def say_hello(first_name)
+  puts "Bonjour, #{first_name} !"
+end
+
+first_name = ask_first_name
+say_hello(first_name)
